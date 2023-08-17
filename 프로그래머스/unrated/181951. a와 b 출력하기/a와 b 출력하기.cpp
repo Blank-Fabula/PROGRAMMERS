9f4b75d885077d8abc466a5595edeac78bd30819
LEE GI TAEK <fabula.story47@gmail.com>
2023-08-18 08:20:49 +0900

    [unrated] Title: a와 b 출력하기, Time: 1.67 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181951. a와 b 출력하기/a와 b 출력하기.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+    int a, b; cin >> a >> b;
+    cout << "a = " << a << '\n';
+    cout << "b = " << b << '\n';
+return 0; }

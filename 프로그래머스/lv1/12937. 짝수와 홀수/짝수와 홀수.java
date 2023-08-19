e9a2aabd4859d4ede4ac809b786bca457b5f98b0
LEE GI TAEK <fabula.story47@gmail.com>
2023-08-19 23:22:23 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.03 ms, Memory: 78.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.java
@@ -0,0 +1,7 @@
+class Solution {
+    public String solution(int num) {
+        String answer = "";
+        if ((num & 1) == 0) { answer = "Even";
+        } else { answer = "Odd"; }
+    return answer; }
+}

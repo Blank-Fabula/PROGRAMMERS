f5ac33763f95d48fa8b9d61e5f8a3692d6be8a7a
LEE GI TAEK <fabula.story47@gmail.com>
2023-08-20 13:39:22 +0900

    Delete x만큼 간격이 있는 n개의 숫자.cpp

--- 프로그래머스/lv1/12954. x만큼 간격이 있는 n개의 숫자/x만큼 간격이 있는 n개의 숫자.cpp
+++
@@ -1,8 +0,0 @@
-#include <string>
-#include <vector>
-using namespace std;
-
-vector<long long> solution(int x, int n) {
-    vector<long long> answer;
-    for (long long sum = x; n--; sum += x) { answer.push_back(sum); }
-return answer; }

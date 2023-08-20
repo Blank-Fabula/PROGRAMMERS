3c2cd247e51c90c4d9d2329a55f8add60a15b205
LEE GI TAEK <fabula.story47@gmail.com>
2023-08-20 13:26:01 +0900

    Delete 평균 구하기.cpp

--- 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.cpp
+++
@@ -1,9 +0,0 @@
-#include <vector>
-using namespace std;
-
-double solution(vector<int> arr) {
-    double answer = 0;
-    int size = arr.size();
-    for(int i = 0; i < size; i++){ answer += arr[i]; }
-    answer /= size;
-return answer; }

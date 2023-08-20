a7dcedf75cafd5a028a1fb395983ca308cdc5b52
LEE GI TAEK <fabula.story47@gmail.com>
2023-08-20 13:24:51 +0900

    [level 1] Title: 약수의 합, Time: 0.03 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.java
@@ -1,8 +1,8 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        for (int i = n; i > 0; i--) {
-            if (n % i == 0) { answer += i; }
-        }
+        for (int i = (int)Math.sqrt(n); i > 0; i--) {
+            if (n % i == 0) { answer += i + n / i; }
+        } if (n % Math.sqrt(n) == 0) { answer -= Math.sqrt(n); }
     return answer; }
 }
